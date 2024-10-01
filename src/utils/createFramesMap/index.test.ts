6698ef860edd20ee2f11db1e1fff1c9c76873ec4
webblocksapp <mauriver21@gmail.com>
2024-10-01 15:54:18 -0500

    Configure jest

---
+++ src/utils/createFramesMap/index.test.ts
@@ -0,0 +1,7 @@
+import { createFramesMap } from '@utils/createFramesMap';
+
+describe('createFramesMap', () => {
+  it('CASE-1', () => {
+    createFramesMap();
+  });
+});
---
+++ src/utils/createFramesMap/index.ts
@@ -0,0 +1,3 @@
+export const createFramesMap = () => {
+  console.log('Hi');
+};
