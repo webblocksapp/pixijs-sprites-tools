5f214cb48554bb864f17de4efd6c179d2b4211db
webblocksapp <mauriver21@gmail.com>
2024-10-01 18:02:07 -0500

    Add MapCreationFromInput story.

--- src/utils/createFramesMap/index.stories.tsx
+++ src/utils/createFramesMap/index.stories.tsx
@@ -45,3 +45,92 @@ export const Overview: Story = {
     );
   },
 };
+
+export const MapCreationFromInput: Story = {
+  render: () => {
+    const [framesMap, setFramesMap] = useState<FramesMap>();
+    const [imgUrl, setImgUrl] = useState('');
+    const [data, setData] = useState<{
+      numRows: number;
+      numCols: number;
+      image: Blob | undefined;
+      imageExtension: string | undefined;
+    }>({
+      numRows: 0,
+      numCols: 0,
+      image: undefined,
+      imageExtension: undefined,
+    });
+
+    const onInput: React.FormEventHandler<HTMLInputElement> = (event) => {
+      const { name, value } = event.currentTarget;
+      setData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    };
+
+    const onInputFile: React.FormEventHandler<HTMLInputElement> = (event) => {
+      const file = event.currentTarget.files?.[0];
+
+      if (file) {
+        const image = new Blob([file], { type: file.type });
+        const imageExtension = file.type.split('/')[1];
+        setImgUrl(URL.createObjectURL(file));
+        setData((prev) => ({
+          ...prev,
+          image,
+          imageExtension,
+        }));
+      }
+    };
+
+    const generateMap = async () => {
+      data.image &&
+        data.imageExtension &&
+        setFramesMap(
+          await createFramesMap({
+            image: data.image,
+            imageExtension: data.imageExtension,
+            numCols: data.numCols,
+            numRows: data.numRows,
+          })
+        );
+    };
+
+    return (
+      <div>
+        <div>
+          <label>Image</label>
+          <input type="file" onInput={onInputFile} />
+        </div>
+        <div>
+          <label>Rows</label>
+          <input
+            name="numRows"
+            type="number"
+            value={data.numRows}
+            onInput={onInput}
+          />
+        </div>
+        <div>
+          <label>Columns</label>
+          <input
+            name="numCols"
+            type="number"
+            value={data.numCols}
+            onInput={onInput}
+          />
+        </div>
+        <button onClick={generateMap}>Generate Map</button>
+        <img src={imgUrl} />
+        <div>
+          <p>Frames map:</p>
+          <code>
+            <pre>{JSON.stringify(framesMap, null, 2)}</pre>
+          </code>
+        </div>
+      </div>
+    );
+  },
+};
