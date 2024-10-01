99dbaff0abd173796091e00078444091adae03d1
webblocksapp <mauriver21@gmail.com>
2024-10-01 16:23:46 -0500

    Add test case of getImageDimensions

--- src/utils/getImageDimensions/index.stories.tsx
+++ src/utils/getImageDimensions/index.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { base64ToBlob } from '@utils/base64ToBlob';
 import { getImageDimensions } from '@utils/getImageDimensions';
-import { image } from './mocks';
+import { image1, image2 } from './mocks';
 import { useEffect, useState } from 'react';
 
 const meta: Meta<typeof getImageDimensions> = {
@@ -13,16 +13,25 @@ export default meta;
 
 export const Overview: Story = {
   render: () => {
-    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+    const [dimensions1, setDimensions1] = useState({ width: 0, height: 0 });
+    const [dimensions2, setDimensions2] = useState({ width: 0, height: 0 });
 
     useEffect(() => {
       (async () => {
-        setDimensions(
-          await getImageDimensions(base64ToBlob(image, 'image/png'))
+        setDimensions1(
+          await getImageDimensions(base64ToBlob(image1, 'image/png'))
+        );
+        setDimensions2(
+          await getImageDimensions(base64ToBlob(image2, 'image/png'))
         );
       })();
     }, []);
 
-    return <>{JSON.stringify(dimensions)}</>;
+    return (
+      <div>
+        <p>Image 1: {JSON.stringify(dimensions1)}</p>
+        <p>Image 2: {JSON.stringify(dimensions2)}</p>
+      </div>
+    );
   },
 };
